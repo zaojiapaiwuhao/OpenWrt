7fd399f2ca74e7566f8abf30884e5c7b0b07263e
zaojiapaiwuhao <35948451+zaojiapaiwuhao@users.noreply.github.com>
2021-12-08 18:44:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git ikoolproxy https://github.com/iwrt/luci-app-ikoolproxy' >>feeds.conf.default
+git clone https://github.com/iwrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
